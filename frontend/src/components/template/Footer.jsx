9534b70d12b648febd9d23a5f77edf04c852d5a2
Larisse <larisse.lima2@gmail.com>
2020-10-23 19:59:50 -0300

    Edita componente Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Lariss<span className="text-danger">e</span>r</strong>
+        </span>
     </footer>
