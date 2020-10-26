1a16d58a227d21e676cc513275c52329c6709c75
Larisse <larisse.lima2@gmail.com>
2020-10-26 20:01:09 -0300

    Adiciona responsividade, e ajustes css

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -5,6 +5,6 @@ export default props =>
     <footer className="footer">
         <span>
             Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> Lariss<span className="text-danger">e</span>r</strong>
+            <strong> Lariss<span className="text-danger">e</span></strong>
         </span>
     </footer>
